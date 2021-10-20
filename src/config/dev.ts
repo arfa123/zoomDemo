f91d6fb4744386b3cc61480039ea714fd8b84d1c
arfa ahmed <arfa.ahmed@nextgeni.com>
2021-10-20 14:34:07 +0500

    zoom demo app

---
+++ src/config/dev.ts
@@ -0,0 +1,9 @@
+import { getExploreName } from '../utils/platform';
+
+export const devConfig = {
+  sdkKey: '',
+  sdkSecret: '',
+  topic: '',
+  name: "",
+  signature: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoidHE5QjJnU0pJMXp5Q2FyYXdGMWlkcjdDc2R6RTNNUURsbXVqIiwidmVyc2lvbiI6MSwidXNlcl9pZGVudGl0eSI6IndlYjEyMyIsInRwYyI6InNlc3Npb25fbmFtZSIsImlhdCI6MTYzNDcxMDIyMiwiZXhwIjoxNjM0Nzk2NTQzfQ.KFrIHP6oqavPkUs8j4r5TL3wYimtlGY0DF9FqXi2TBQ',
+};
